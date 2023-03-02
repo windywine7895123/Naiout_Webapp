d74a4ebbfbaa3ef212063c1f587773b64deb6d21
thanchanok hongsuwanwattana <Than2544@hotmail.co.th>
2023-03-02 22:10:23 +0700

    crud_front

--- front_end/src/app/service/crud.service.ts
+++ front_end/src/app/service/crud.service.ts
@@ -22,10 +22,60 @@ export class CrudService {
   //Node/Express API
   REST_API: string = 'http://localhost:3000/book';
 
-  httpHeader = new HttpHeaders().set('Content-Type', 'application/json');
+  httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(private httpClient: HttpClient) { }
 
   //Add
-  Addb
+  AddBook(data: Book): Observable<any> {
+    let API_URL = `${this.REST_API}/home`;
+    return this.httpClient.post(API_URL, data)
+      .pipe(
+        catchError(this.handleError)
+      )
+  }
+
+  //Get all objects
+  GetBooks() {
+    return this.httpClient.get(`${this.REST_API}`)
+  }
+
+  //Get single object
+  GetBook(id: any): Observable<any>{
+    let API_URL = `${this.REST_API}/read-book/${id}`;
+    return this.httpClient.get(API_URL, { headers: this.httpHeaders})
+      .pipe(map((res: any) => {
+        return res || {}
+      }),
+      catchError(this.handleError)
+      )
+  }
+
+  //Update
+  updateBook(id: any, data:any): Observable<any> {
+    let API_URL = `${this.REST_API}/update-book/${id}`;
+    return this.httpClient.put(API_URL, data, {headers: this.httpHeaders})
+  }
+
+  //Delete
+  deleteBook(id: any): Observable<any>{
+    let API_URL = `${this.REST_API}/delete-book/${id}`;
+    return this.httpClient.delete(API_URL, {headers: this.httpHeaders})
+      .pipe(
+        catchError(this.handleError)
+      )
+  }
+  //Error
+  handleError(error: HttpErrorResponse){
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      //handle client error
+      errorMessage = error.error.message;
+    } else {
+      //handle server error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage)
+  }
 }
