665e9cb25de29dc4567579d4ff24d7da5ef30b51
thanchanok hongsuwanwattana <Than2544@hotmail.co.th>
2023-03-03 23:33:06 +0700

    home

--- front_end/src/app/home/home.component.ts
+++ front_end/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CrudService, Book } from '../service/crud.service';
+import { CrudService } from '../service/crud.service';
 
 @Component({
   selector: 'app-home',
