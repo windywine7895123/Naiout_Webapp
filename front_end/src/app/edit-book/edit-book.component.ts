96b804c45b574397ac6e4b1c31a18cae4ef88185
NutchaDan <nutcha.siriwut@gmail.com>
2023-03-10 22:01:30 +0700

    edit book

--- front_end/src/app/edit-book/edit-book.component.ts
+++ front_end/src/app/edit-book/edit-book.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router'
+import { CrudService } from '../service/crud.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-edit-book',
   templateUrl: './edit-book.component.html',
@@ -7,4 +10,38 @@ import { Component } from '@angular/core';
 })
 export class EditBookComponent {
 
+  book: any; //for test code
+
+  bookForm: FormGroup;
+ 
+
+  constructor(
+    public formBuilder: FormBuilder,
+    private router: Router,
+    private ngZone: NgZone,
+    private crudService: CrudService,
+
+  ) { 
+    this.bookForm = this.formBuilder.group({
+      book_name: [''],
+      book_image: [''],
+      book_type: [''], 
+      book_writer: [''],
+      book_publisher: [''],
+      book_item: [''],
+      book_price: [''],
+      book_detail: [''],
+    })
+   }
+
+  ngOnInit(): void {
+  }
+  onSubmit(): any {
+    this.crudService.AddBook(this.bookForm.value)
+    .subscribe(()=>{
+        console.log("Book added successfully");
+        this.ngZone.run(() => this.router.navigateByUrl('/list'))
+      })
+  }
 }
+
